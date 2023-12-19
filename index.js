f51e36e4af16e4790f00de5b72f1b19f5d4589b5
Victor <viqtorkibz@gmail.com>
2023-12-19 11:53:46 +0300

    completed assynment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js
+
+// Function to handle scuberGreetingForFeet
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+      return 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+      return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to handle ternaryCheckCity
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to handle switchOnCharmFromTip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
+
+// Exporting functions for testing
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip
+};
